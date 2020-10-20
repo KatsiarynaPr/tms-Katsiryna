604f94032a7cc25ea31303145fb7b833b42bd51f
Katsiaryna <katarina.pr@mail.ru>
2020-10-20 22:01:43 +0300

    v1_15_10_2020

--- src/framework/wsgi.py
+++ src/framework/wsgi.py
@@ -13,6 +13,16 @@ def application(environ, start_response):
 
         yield payload
 
+    elif url=="/logo.png/":
+        status = "200 OK"
+        headers = {
+            "Content-type": "image/jpeg",
+        }
+        payload = read_from_logo_png()
+        start_response(status, list(headers.items()))
+
+        yield payload
+
     else:
         status = "200 OK"
         headers = {
@@ -42,4 +52,12 @@ def read_from_style_css():
         payload = fp.read()
 
     payload = payload.encode()
+    return payload
+
+def read_from_logo_png():
+    path = DIR_STATIC / "logo.jpg"
+
+    with path.open("rb") as fp:
+        payload = fp.read()
+
     return payload
