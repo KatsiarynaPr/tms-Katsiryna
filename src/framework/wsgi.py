d84a49229aec8e8bf24bd64e3f664a632e64eb1b
KatsiarynaPr <72443596+KatsiarynaPr@users.noreply.github.com>
2020-10-27 19:13:05 +0300

    v3_27_10_2020 - грейт эдвайс заработал (#4)

--- src/framework/wsgi.py
+++ src/framework/wsgi.py
@@ -1,3 +1,4 @@
+import random
 from mimetypes import guess_type
 
 from framework.consts import DIR_STATIC
@@ -6,17 +7,22 @@ from framework.consts import DIR_STATIC
 def application(environ, start_response):
     url = environ["PATH_INFO"]
 
-    file_names = {
-        "/xxx/": "styles.css",
-        "/logo.png/": "logo.jpg",
-    }
-    file_name = file_names.get(url, "index.html")
+    file_names = {"/xxx/": "styles.css", "/logo.png/": "logo.jpg", "/": "index.html"}
+    file_name = file_names.get(url)
 
-    status = "200 OK"
-    headers = {
-        "Content-type": guess_type(file_name)[0],
-    }
-    payload = read_static(file_name)
+    if file_name is not None:
+        status = "200 OK"
+        headers = {
+            "Content-type": guess_type(file_name)[0],
+        }
+        payload = read_static(file_name)
+
+    else:
+        status = "404 Not Found"
+        headers = {
+            "Content-type": "text/html",
+        }
+        payload = generate_404(environ)
 
     start_response(status, list(headers.items()))
 
@@ -30,3 +36,12 @@ def read_static(file_name: str) -> bytes:
         payload = fp.read()
 
     return payload
+
+
+def generate_404(environ) -> bytes:
+    url = environ["PATH_INFO"]
+    pin = random.randint(1, 10000)
+    advice = "https://fucking-great-advice.ru/advice/" + str(pin)
+    msg = f'Your path: {url} not found. Read the advice ---> <a href="{advice}">ADVICE</a> !!!'
+
+    return msg.encode()
