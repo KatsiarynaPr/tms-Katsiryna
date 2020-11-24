4b16b1ca6c4702c014151cb72212a8c2f615b092
Katsiaryna <katarina.pr@mail.ru>
2020-11-24 22:06:58 +0300

    lesson 16 - переезд

--- src/application/hello/urls.py
+++ src/application/hello/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
 
-from application.hello.views import hello
+from application.hello import views
 
 urlpatterns = [
-    path("", hello),
+    path("", views.hello),
+    path("greet/", views.view_hello_greet),
+    path("reset/", views.view_hello_reset),
 ]
--- src/application/hello/views.py
+++ src/application/hello/views.py
@@ -1,8 +1,30 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def hello(request: HttpRequest) -> HttpResponse:
-    result = render(request, "hello/hello.html")
+    name = request.session.get("name")
+    address = request.session.get("address")
+    context = {
+        "name_header": name or "anonymous",
+        "name_value": name or "",
+        "address_header": address or "nowhere",
+        "address_value": address or "",
+    }
+    result = render(request, "hello/hello.html", context=context)
 
     return HttpResponse(result)
+
+def view_hello_greet(request: HttpRequest) -> HttpResponse:
+    name = request.POST.get("name")
+    address = request.POST.get("address")
+
+    request.session["name"] = name
+    request.session["address"] = address
+
+    return redirect("/h/")
+
+
+def view_hello_reset(request: HttpRequest) -> HttpResponse:
+    request.session.clear()
+    return redirect("/h/")
