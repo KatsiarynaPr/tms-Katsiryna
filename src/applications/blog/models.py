2b34e4cc9bc1f67bdce1d518c34137cd5a330c73
Katsiaryna <katarina.pr@mail.ru>
2020-12-20 18:51:06 +0300

    blog with likes on JS+ litel task

--- src/applications/blog/models.py
+++ src/applications/blog/models.py
@@ -1,6 +1,7 @@
 import delorean
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models import Manager
 from django.urls import reverse
 from django.urls import reverse_lazy
 
@@ -11,13 +12,27 @@ def _now():
     return delorean.utcnow().datetime
 
 
+class PostManager(Manager):
+    pass
+
+
 class Post(models.Model):
+    objects = PostManager()
     title = models.TextField(null=True, blank=True)
     content = models.TextField(null=True, blank=True)
     nr_likes = models.IntegerField(default=0)
     created_at = models.DateTimeField(default=_now)
 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likers = models.ManyToManyField(User, related_name="liked_posts", blank=True)
+
+    @property
+    def nr_likes(self):
+        return self.likers.count()
+
+    def is_liked_by(self, user: User) -> bool:
+        liked = Post.objects.filter(pk=self.pk, likers=user).exists()
+        return liked
 
     class Meta:
         ordering = ["-created_at"]
